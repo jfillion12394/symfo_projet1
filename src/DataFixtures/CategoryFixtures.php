cdc94f3e8acb1bc3e9f79e07c7c229c9c6d21d13
jfillion12394 <jfillion12394@gmail.com>
2021-05-31 09:52:31 +0200

    fixtures

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $category = new Category();
+        $category->setName('Horreur');
+        $manager->persist($category);
+
+        $category = new Category();
+        $category->setName('Action');
+        $manager->persist($category);
+
+        $category = new Category();
+        $category->setName('Adventure');
+        $manager->persist($category);
+
+        $manager->flush();
+    }
+
+  
+
+}
